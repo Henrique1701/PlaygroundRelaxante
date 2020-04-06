6c9e2f01fb397033a0b3a1b6585ccee783b3a778
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2020-04-06 18:25:50 -0300

    Adicionando música de fundo

--- PlaygroundRelaxanteSingleView.playground/Pages/Tela principal.xcplaygroundpage/Contents.swift
+++ PlaygroundRelaxanteSingleView.playground/Pages/Tela principal.xcplaygroundpage/Contents.swift
@@ -2,14 +2,35 @@
   
 import UIKit
 import PlaygroundSupport
+import AVFoundation
 
-class FirstViewController : UIViewController, UITextFieldDelegate {
+private var chompPlayer: AVAudioPlayer?
+chompPlayer = createPlayer(from: "som tema floresta")
+chompPlayer?.play()
+func createPlayer(from filename: String) -> AVAudioPlayer? {
+    guard let url = Bundle.main.url(forResource: filename, withExtension: "mp3") else {
+        return nil
+    }
+    var player = AVAudioPlayer()
+    do {
+        try player = AVAudioPlayer(contentsOf: url)
+        player.prepareToPlay()
+    } catch {
+        print("Error loading \(url.absoluteString): \(error)")
+    }
+    
+    return player
+}
+
+var nomeUsuario = ""
+
+class PrimeiraViewController : UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    
     
     let botaoPararIniciarMusica = UIButton()
     
     let textFieldSeuNome = UITextField()
-    var nomeUsuario = ""
-    
     
     override func loadView() {
         let view = UIView()
@@ -35,6 +56,7 @@ class FirstViewController : UIViewController, UITextFieldDelegate {
         botaoPararIniciarMusica.frame = CGRect(x: 1260, y: 57, width: 164, height: 35)
         let imagemBotaoMusica = #imageLiteral(resourceName: "Parar música.png")
         botaoPararIniciarMusica.setBackgroundImage(imagemBotaoMusica, for: .normal)
+        botaoPararIniciarMusica.addTarget(self, action: #selector(pararIniciarMusica), for: .touchUpInside)
         
         // Caixa de texto, com label
         let imagemCaixaTexto = #imageLiteral(resourceName: "CaixaDeTexto.png")
@@ -46,6 +68,11 @@ class FirstViewController : UIViewController, UITextFieldDelegate {
         textViewConversa.text = "Qual é o seu nome ?"
         textViewConversa.textAlignment = .center
         
+        caixaTextoView.isUserInteractionEnabled = true
+        let tocouCaixaTexto = UITapGestureRecognizer(target: self, action: #selector(clicouCaixaTexto))
+        caixaTextoView.addGestureRecognizer(tocouCaixaTexto)
+        
+        
         // Text field seu nome
         textFieldSeuNome.frame = CGRect(x: 394, y: 701, width: 651, height: 106)
         textFieldSeuNome.background = #imageLiteral(resourceName: "ImagemTextField.png")
@@ -56,9 +83,19 @@ class FirstViewController : UIViewController, UITextFieldDelegate {
         textFieldSeuNome.addTarget(self, action: #selector(recebeNome), for: .editingChanged)
         
         // Botão caixa de texto
-        let botaoCaixaTexto = UIButton(frame: CGRect(x: 394, y: 221, width: 651, height: 458))
-        botaoCaixaTexto.backgroundColor = UIColor.transparente()
-        botaoCaixaTexto.addTarget(nil, action: #selector(clicouCaixaTexto), for: .touchUpInside)
+//        let botaoCaixaTexto = UIButton(frame: CGRect(x: 394, y: 221, width: 651, height: 458))
+//        botaoCaixaTexto.backgroundColor = UIColor.transparente()
+//        botaoCaixaTexto.addTarget(nil, action: #selector(clicouCaixaTexto), for: .touchUpInside)
+        
+        // Testando movimento de itens
+        let animal = UIImage(named: "animal.jpeg")
+        let animalView = UIImageView(image: animal)
+        animalView.frame = CGRect(x: 50, y: 50, width: 200, height: 200)
+        
+        animalView.isUserInteractionEnabled = true
+        let panAnimal = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        animalView.addGestureRecognizer(panAnimal)
+        
         
         // Adiciona Subviews
         view.addSubview(backgroundView)
@@ -67,7 +104,8 @@ class FirstViewController : UIViewController, UITextFieldDelegate {
         view.addSubview(textViewConversa)
         view.addSubview(botaoMudarTema)
         view.addSubview(botaoPararIniciarMusica)
-        view.addSubview(botaoCaixaTexto)
+        //view.addSubview(botaoCaixaTexto)
+        view.addSubview(animalView)
         self.view = view
         
     }
@@ -77,16 +115,43 @@ class FirstViewController : UIViewController, UITextFieldDelegate {
         print("Clicou na caixa de texto")
         //show(secondViewController, sender: nil)
         //present(secondViewController, animated: true, completion: nil)
-        navigationController?.pushViewController(secondViewController, animated: true)
+        navigationController?.pushViewController(segundaViewController, animated: true)
     }
     
     @objc func recebeNome() {
-        self.nomeUsuario = textFieldSeuNome.text!
+        nomeUsuario = textFieldSeuNome.text!
+    }
+    
+    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else {
+            return
+        }
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + translation.x,
+            y: gestureView.center.y + translation.y
+        )
+        gesture.setTranslation(.zero, in: view)
+    }
+    
+    @objc func pararIniciarMusica() {
+        if chompPlayer?.isPlaying == true {
+            chompPlayer?.pause()
+            let imagemIniciarMusica = #imageLiteral(resourceName: "Inicar música.png")
+            botaoPararIniciarMusica.setBackgroundImage(imagemIniciarMusica, for: .normal)
+        } else {
+            chompPlayer?.play()
+            let imagemPararMusica = #imageLiteral(resourceName: "Parar música.png")
+            botaoPararIniciarMusica.setBackgroundImage(imagemPararMusica, for: .normal)
+        }
+        
     }
     
 }
 
-class SecondViewController: UIViewController {
+class SegundaViewController: UIViewController {
+    
+    let botaoPararIniciarMusica = UIButton()
     
     override func loadView() {
         let view = UIView()
@@ -104,7 +169,6 @@ class SecondViewController: UIViewController {
         botaoMudarTema.setBackgroundImage(imagemBotaoMudarTema, for: .normal)
         
         // Botão parar música
-        let botaoPararIniciarMusica = UIButton()
         botaoPararIniciarMusica.frame = CGRect(x: 1260, y: 57, width: 164, height: 35)
         let imagemBotaoMusica = #imageLiteral(resourceName: "Parar música.png")
         botaoPararIniciarMusica.setBackgroundImage(imagemBotaoMusica, for: .normal)
@@ -116,16 +180,134 @@ class SecondViewController: UIViewController {
         
         let textViewConversa = UITextView(frame: CGRect(x: 545, y: 375, width: 350, height: 179))
         textViewConversa.backgroundColor = UIColor.transparente()
-        textViewConversa.text = "Então, 'Seu nome', não sei o que está causando a sua ansiedade."
+        textViewConversa.text = "Então, \(nomeUsuario), não sei o que está causando a sua ansiedade."
         textViewConversa.textAlignment = .center
+        
+        // Botão caixa de texto
+        let botaoCaixaTexto = UIButton(frame: CGRect(x: 394, y: 221, width: 651, height: 458))
+        botaoCaixaTexto.backgroundColor = UIColor.transparente()
+        botaoCaixaTexto.addTarget(nil, action: #selector(clicouCaixaTexto), for: .touchUpInside)
         
         // Adiciona Subviews
         view.addSubview(backgroundView)
-        view.addSubview(botaoMudarTema)
-        view.addSubview(botaoPararIniciarMusica)
         view.addSubview(caixaTextoView)
         view.addSubview(textViewConversa)
+        view.addSubview(botaoMudarTema)
+        view.addSubview(botaoPararIniciarMusica)
+        view.addSubview(botaoCaixaTexto)
         self.view = view
+    }
+    
+    @objc func clicouCaixaTexto() {
+        navigationController?.pushViewController(terceiraViewController, animated: true)
+    }
+}
+
+class TerceiraViewController: UIViewController {
+    
+    let botaoPararIniciarMusica = UIButton()
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .systemTeal
+        
+        // Imagem de fundo (tema)
+        let backgroundImagem = #imageLiteral(resourceName: "tema01.png")
+        let backgroundView = UIImageView(image: backgroundImagem)
+        backgroundView.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
+        
+        // Botão mudar tema
+        let botaoMudarTema = UIButton()
+        botaoMudarTema.frame = CGRect(x: 1260, y: 13, width: 164, height: 35)
+        let imagemBotaoMudarTema = #imageLiteral(resourceName: "Mudar tema.png")
+        botaoMudarTema.setBackgroundImage(imagemBotaoMudarTema, for: .normal)
+        
+        // Botão parar música
+        botaoPararIniciarMusica.frame = CGRect(x: 1260, y: 57, width: 164, height: 35)
+        let imagemBotaoMusica = #imageLiteral(resourceName: "Parar música.png")
+        botaoPararIniciarMusica.setBackgroundImage(imagemBotaoMusica, for: .normal)
+        
+        // Caixa de texto, com label
+        let imagemCaixaTexto = #imageLiteral(resourceName: "CaixaDeTexto.png")
+        let caixaTextoView = UIImageView(image: imagemCaixaTexto)
+        caixaTextoView.frame = CGRect(x: 394, y: 221, width: 651, height: 458)
+        
+        let textViewConversa = UITextView(frame: CGRect(x: 545, y: 375, width: 350, height: 179))
+        textViewConversa.backgroundColor = UIColor.transparente()
+        textViewConversa.text = "Mas, estou aqui para te ajudar a controlar essa ansiedade !"
+        textViewConversa.textAlignment = .center
+        
+        // Botão caixa de texto
+        let botaoCaixaTexto = UIButton(frame: CGRect(x: 394, y: 221, width: 651, height: 458))
+        botaoCaixaTexto.backgroundColor = UIColor.transparente()
+        botaoCaixaTexto.addTarget(nil, action: #selector(clicouCaixaTexto), for: .touchUpInside)
+        
+        // Adiciona Subviews
+        view.addSubview(backgroundView)
+        view.addSubview(caixaTextoView)
+        view.addSubview(textViewConversa)
+        view.addSubview(botaoMudarTema)
+        view.addSubview(botaoPararIniciarMusica)
+        view.addSubview(botaoCaixaTexto)
+        self.view = view
+    }
+    
+    @objc func clicouCaixaTexto() {
+        navigationController?.pushViewController(quartaViewController, animated: true)
+    }
+}
+
+class QuartaViewController: UIViewController {
+    
+    let botaoPararIniciarMusica = UIButton()
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .systemTeal
+        
+        // Imagem de fundo (tema)
+        let backgroundImagem = #imageLiteral(resourceName: "tema01.png")
+        let backgroundView = UIImageView(image: backgroundImagem)
+        backgroundView.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
+        
+        // Botão mudar tema
+        let botaoMudarTema = UIButton()
+        botaoMudarTema.frame = CGRect(x: 1260, y: 13, width: 164, height: 35)
+        let imagemBotaoMudarTema = #imageLiteral(resourceName: "Mudar tema.png")
+        botaoMudarTema.setBackgroundImage(imagemBotaoMudarTema, for: .normal)
+        
+        // Botão parar música
+        botaoPararIniciarMusica.frame = CGRect(x: 1260, y: 57, width: 164, height: 35)
+        let imagemBotaoMusica = #imageLiteral(resourceName: "Parar música.png")
+        botaoPararIniciarMusica.setBackgroundImage(imagemBotaoMusica, for: .normal)
+        
+        // Caixa de texto, com label
+        let imagemCaixaTexto = #imageLiteral(resourceName: "CaixaDeTexto.png")
+        let caixaTextoView = UIImageView(image: imagemCaixaTexto)
+        caixaTextoView.frame = CGRect(x: 394, y: 221, width: 651, height: 458)
+        
+        let textViewConversa = UITextView(frame: CGRect(x: 545, y: 375, width: 350, height: 179))
+        textViewConversa.backgroundColor = UIColor.transparente()
+        textViewConversa.text = "O que você gostaria de fazer agora ?"
+        textViewConversa.textAlignment = .center
+        
+        // Botão caixa de texto
+        let botaoCaixaTexto = UIButton(frame: CGRect(x: 394, y: 221, width: 651, height: 458))
+        botaoCaixaTexto.backgroundColor = UIColor.transparente()
+        botaoCaixaTexto.addTarget(nil, action: #selector(clicouCaixaTexto), for: .touchUpInside)
+        
+        // Adiciona Subviews
+        view.addSubview(backgroundView)
+        view.addSubview(caixaTextoView)
+        view.addSubview(textViewConversa)
+        view.addSubview(botaoMudarTema)
+        view.addSubview(botaoPararIniciarMusica)
+        view.addSubview(botaoCaixaTexto)
+        self.view = view
+    }
+    
+    @objc func clicouCaixaTexto() {
+        
     }
 }
 
@@ -136,14 +318,16 @@ extension UIColor {
 }
 
 // Present the view controller in the Live View window
-let firstViewController = FirstViewController(screenType: .mac, isPortrait: true)
-let secondViewController = SecondViewController(screenType: .mac, isPortrait: true)
+let primeiraViewController = PrimeiraViewController(screenType: .mac, isPortrait: true)
+let segundaViewController = SegundaViewController(screenType: .mac, isPortrait: true)
+let terceiraViewController = TerceiraViewController(screenType: .mac, isPortrait: true)
+let quartaViewController = QuartaViewController(screenType: .mac, isPortrait: true)
 
 let navigation = UINavigationController(screenType: .mac, isPortrait: true)
 navigation.navigationBar.isHidden = true
-navigation.pushViewController(firstViewController, animated: true)
+navigation.pushViewController(primeiraViewController, animated: true)
 
-PlaygroundPage.current.liveView = firstViewController.scale(to: 0.5)
+PlaygroundPage.current.liveView = navigation.scale(to: 0.5)
 
 //: # Navegação
 
